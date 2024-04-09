5a45f034178464dab45c93b01eb3bc303aec78ac
laisuk <laisuk@yahoo.com>
2024-04-09 16:20:57 +0800

    Add capi for demo-linux-amd64

---
+++ capi/demo-linux-amd64/opencc_fmmseg_capi.h
@@ -0,0 +1,22 @@
+#ifndef OPENCC_FMMSEG_CAPI_H
+#define OPENCC_FMMSEG_CAPI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+
+void *opencc_new();
+char *opencc_convert(const void *instance, const char *config, const char *input, bool punctuation);
+bool opencc_get_parallel(const void *instance);
+void opencc_set_parallel(const void *instance, bool is_parallel);
+int opencc_zho_check(const void *instance, const char *input);
+void opencc_free(const void *instance);
+void opencc_string_free(const char *ptr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // OPENCC_FMMSEG_CAPI_H
