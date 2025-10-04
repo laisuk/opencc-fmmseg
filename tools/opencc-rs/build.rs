6c373144df61865715e494941071dd7e599c901a
laisuk <laisuk@yahoo.com>
2025-10-05 06:07:50 +0800

    Add opencc-rs.exe icon

--- tools/opencc-rs/build.rs
+++ tools/opencc-rs/build.rs
@@ -5,7 +5,7 @@ fn main() {
         res.set_icon("assets/icon.ico");
 
         let ver = std::env::var("CARGO_PKG_VERSION").unwrap_or_else(|_| "0.0.0.0".into());
-        res.set("FileDescription", "opencc-fmmseg CLI");
+        res.set("FileDescription", "Opencc-Fmmseg CLI");
         res.set("ProductName", "opencc-rs");
         res.set("FileVersion", &ver);
         res.set("ProductVersion", &ver);
